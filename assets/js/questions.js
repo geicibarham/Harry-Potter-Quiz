eefff27c5217cf941f96fcc9446665b731837b56
geicibarham <geicianecosta43@gmail.com>
2022-04-21 13:26:30 -0400

    first commit

---
+++ assets/js/questions.js
@@ -0,0 +1,38 @@
+
+    const questions = [
+     
+        {
+            title: "What is the name of Harry's dad?",
+            choices: ["Dumbledore", "James Potter", "Sirius Black Potter", "Remulo Potter"],
+            answer: "James Potter"
+        },
+        {
+            title: "Which house is Ollivander in?",
+            choices: ["Ravenclaw", "Gryffindor", "Hufflepuff", "Slytherin"],
+            answer: "Ravenclaw"
+        },
+        {
+            title: "Where did Harry and Ron meet?",
+            choices: ["Hogwarts", "King's Cross Station", "Hogwartz train", "Commom Room"],
+            answer: "King's Cross Station"
+        },
+        {
+            title: "Who brought Harry to the Dursleys?",
+            choices: ["Albus Dumbledore" , "Hagrid" , "Minerva Mcgonagall", "Sirius Black"],
+            answer: "Albus Dumbledore"
+        } ,
+
+
+        {
+            title: "What spell did Hermione use to free Harry and Ron from the Devil's Snare?",
+            choices: ["Accio", "Lumos Solem Spell","Alohomora", "Expelliarmus"],
+            answer: "Lumos Solem Spell"
+        },
+
+        {
+            title: "  What spell created by the Half Blood Prince did Harry use on Draco Malfoy?",
+            choices: ["Ascendio", "Obliviate","Sectumsempra", "Protego"],
+            answer: "Sectumsempra"
+        },
+      
+    ];
